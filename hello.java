ce6ee295678d04361e95aa67b098f81fda8a9106
Sumedh Shekhar <sumedh@sumedh-ThinkPad-L420.(none)>
2013-03-02 18:06:42 +0530

    FirstCommit

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class hello
+{
+	public static void main(String a[])
+	{
+		System.out.print("Enter an integer");
+		Scanner S=new Scanner(System.in);
+		int p=S.nextInt();
+		System.out.println("The integer is "+p);
+	}
+}
+
